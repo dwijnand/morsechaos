f45fed9fd3eb24a5bd9696b7b97329fc33c949cf
Dale Wijnand <dale.wijnand@gmail.com>
2015-10-03 17:04:38 +0100

    Add morse.txt & implement Morse

--- src/main/scala/morsechaos/Morse.scala
+++ src/main/scala/morsechaos/Morse.scala
@@ -1,3 +1,15 @@
 package morsechaos
 
-object Morse
+object Morse {
+  val charToMorse = Map(
+    ('a', ".-"), ('b', "-..."), ('c', "-.-."), ('d', "-.."), ('e', "."), ('f', "..-."), ('g', "--."),
+    ('h', "...."), ('i', ".."), ('j', ".---"), ('k', "-.-"), ('l', ".-.."), ('m', "--"), ('n', "-."),
+    ('o', "---"), ('p', ".--."), ('q', "--.-"), ('r', ".-."), ('s', "..."), ('t', "-"), ('u', "..-"),
+    ('v', "...-"), ('w', ".--"), ('x', "-..-"), ('y', "-.--"), ('z', "--..")
+  )
+
+  val morseToChar = charToMorse.map(_.swap)
+
+  def encode(ch: Char): Option[String] = charToMorse get ch
+  def decode(s: String): Option[Char]  = morseToChar get s
+}
