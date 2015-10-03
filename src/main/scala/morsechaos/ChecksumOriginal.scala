8c529eef7f4912332ccea4a3224d1f1c8849ae2d
Dale Wijnand <dale.wijnand@gmail.com>
2015-10-03 16:08:40 +0100

    Implement ChecksumOriginal

---
+++ src/main/scala/morsechaos/ChecksumOriginal.scala
@@ -0,0 +1,35 @@
+package morsechaos
+
+import scala.collection.JavaConverters._
+import java.io._
+import java.security.MessageDigest
+import java.nio.file._
+
+object ChecksumOriginal {
+  def main(args: Array[String]): Unit = {
+    val bytes = Files.readAllBytes(Paths.get("signs.txt"))
+
+    val incomingMd5 = Files.readAllLines(Paths.get("signs.txt.md5")).asScala.head.split(' ').head
+    val expectedMd5 = calcMd5(bytes)
+    require(incomingMd5 == expectedMd5, s"MD5 missmatch: $incomingMd5 != $expectedMd5")
+
+    val incomingSha1 = Files.readAllLines(Paths.get("signs.txt.sha1")).asScala.head.split(' ').head
+    val expectedSha1 = calcSha1(bytes)
+    require(incomingSha1 == expectedSha1, s"SHA1 missmatch: $incomingSha1 != $expectedSha1")
+
+    println("OK, checksums match")
+  }
+
+  def calcMd5(bytes: Array[Byte]): String    = calcCksum(md5er, bytes)
+  def calcSha1(bytes: Array[Byte]): String   = calcCksum(sha1er, bytes)
+  def calcSha256(bytes: Array[Byte]): String = calcCksum(sha256er, bytes)
+
+  private def calcCksum(md: MessageDigest, bytes: Array[Byte]): String = { md update bytes ; hex(md.digest) }
+
+  def md5er    = MessageDigest getInstance "MD5"
+  def sha1er   = MessageDigest getInstance "SHA-1"
+  def sha256er = MessageDigest getInstance "SHA-256"
+
+  /** Returns the hexadecimal string form of the specified byte array. */
+  def hex(bytes: Array[Byte]) = bytes.map(b => "%02x" format b) mkString ""
+}
