1da0e6496b0a3963a2823aff6aafb75e4fe775c7
Dale Wijnand <dale.wijnand@gmail.com>
2015-10-12 01:32:15 +0100

    Fix ConvertCount2w

--- src/main/scala/morsechaos/ConvertCount2w.scala
+++ src/main/scala/morsechaos/ConvertCount2w.scala
@@ -11,8 +11,8 @@ object ConvertCount2w {
     val split1 = lines1.map { l => val sp = l split '\t'; val sp2 = sp(0) split ' '; (sp2(0), sp2(1), sp(1).toLong) }
     val split2 = split1.map { case (w1, w2, count) => (w1.toLowerCase, w2.toLowerCase, count) }
     val split3 = distinctBy(split2, (t: (String, String, Long)) => (t._1, t._2))
-    val split4 = split3.filter { case (w1, _, _) => !Set("i", "a").contains(w1) && w1.length > 1 }
-    val split5 = split4.filter { case (_, w2, _) => !Set("i", "a").contains(w2) && w2.length > 1 }
+    val split4 = split3.filter { case (w1, _, _) => Set("i", "a").contains(w1) || w1.length > 1 }
+    val split5 = split4.filter { case (_, w2, _) => Set("i", "a").contains(w2) || w2.length > 1 }
     val split6 = split5.filter { case (w1, _, _) => w1.forall(ch => Morse.charToMorse.keySet.contains(ch)) }
     val split7 = split6.filter { case (_, w2, _) => w2.forall(ch => Morse.charToMorse.keySet.contains(ch)) }
     val split8 = split7.map { case (w1, w2, count) =>
